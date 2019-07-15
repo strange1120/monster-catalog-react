42fb1bc254b44b9b83b5e5c30a8db24b34a0124e
Sarah Strange <sarah.strange@availity.com>
2019-07-15 09:08:38 -0400

    missing commit from before the weekend

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchFiled: ""
     };
   }
   componentDidMount() {
@@ -19,6 +20,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
         <CardList monsters={this.state.monsters} />
       </div>
     );
